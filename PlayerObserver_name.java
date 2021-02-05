64f4f9a72ab141b66689f2f68f0dc17fe72e724b
Nlakshitha629 <N.L.Yakupitiyage@wlv.ac.uk>
2021-02-05 22:02:49 +0530

    Desing_pattern_MVC_&Observer_part2

---
+++ PlayerObserver_name.java
@@ -0,0 +1,25 @@
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class PlayerObserver_name extends Observer{
+	
+	   public PlayerObserver_name(Subject subject){
+	      this.subject = subject;
+	      this.subject.attach(this);
+	   }
+
+	   @Override
+	   public void displayFrame() {
+			label = new JLabel(MultiLinugualStringTable.getMessage(0));
+			welcomeFrame.add(label);
+			text = new JTextField(16); 
+		    welcomeFrame.add(text);
+		    button = new JButton("Ok");
+			welcomeFrame.add(button);
+			button.addActionListener(this); 
+			welcomeFrame.pack();
+	   }
+
+}
